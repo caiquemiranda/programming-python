7abe773435f7c6e30ed2028853d89e0b1815d323
caiquemiranda <caiquemirandas@gmail.com>
2022-09-04 21:34:23 -0300

    pallidrome

--- pallidrome.py
+++ pallidrome.py
@@ -1,3 +1,15 @@
-n = 
+n=int(input("Enter number:"))
 
-continue...
+temp=n
+rev=0
+
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+
+if(temp==rev):
+    print("The number is a palindrome!")
+
+else:
+    print("The number isn't a palindrome")
