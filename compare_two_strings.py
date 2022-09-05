95f2928ed3007f4b70d437386883b99c56ebcf8e
caiquemiranda <caiquemirandas@gmail.com>
2022-09-04 21:41:26 -0300

    compare_two_strings

---
+++ compare_two_strings.py
@@ -0,0 +1,11 @@
+str1 = input("Enter String 1  ")
+str2 = input("Enter String 2  ")
+
+if(str1 != ""):
+    if(str1 == str2):
+        print("string is Equal")
+    else:
+        print("string is not Equal")
+
+else:
+    print("pls Enter String to check !!!!!!!1")
