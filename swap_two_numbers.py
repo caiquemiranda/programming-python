fd5eee2b571814ae73ea05f992b66d00a720d961
caiquemiranda <caiquemirandas@gmail.com>
2022-09-07 22:29:21 -0300

    swap numbers

---
+++ swap_two_numbers.py
@@ -0,0 +1,13 @@
+num_1 = input('Enter first number: ')
+num_2 = input('Enter second number: ')
+
+print(f'Value of number before swapping: {num_1}')
+print(f'Value of number before swapping: {num_2}')
+
+temp = num_1
+num_1 = num_2
+num_2 = temp
+
+print('-------------------------------------------------')
+print(f'Value of number after swapping: {num_1}')
+print(f'Value of number after swapping: {num_2}')
