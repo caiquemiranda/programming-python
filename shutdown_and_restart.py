d2e922b5c6b894acbbe6de35944ae040291188bb
caiquemiranda <caiquemirandas@gmail.com>
2022-09-08 21:29:48 -0300

    shutdown

---
+++ shutdown_and_restart.py
@@ -0,0 +1,13 @@
+from logging import shutdown
+from multiprocessing.spawn import import_main_path
+
+
+import os
+
+print('yes or no')
+shutdown = input('Do you wish to shutdown your computer? ')
+
+if (shutdown == 'no'):
+    exit()
+else:
+    os.system('shutdown /s /t 1')
