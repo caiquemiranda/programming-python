727ed1e8ab05196a052ce336b57cef44d2c75127
caiquemiranda <caiquemirandas@gmail.com>
2022-09-28 21:36:25 -0300

    largest_number

---
+++ b2/largest_number.py
@@ -0,0 +1,7 @@
+arr1 = [5, 1, 70, 75, 56, 78, 40]   
+max = arr1[0] 
+for i in range(0, len(arr1)):    
+   if(arr1[i] > max):    
+       max = arr1[i]   
+           
+print("Largest element : " + str(max));
