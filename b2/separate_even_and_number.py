ff46925a2de2bd7457e6dcbcadfe79e04fcd4e5b
caiquemiranda <caiquemirandas@gmail.com>
2022-10-12 10:12:08 -0300

    separate_even_and_odd

---
+++ b2/separate_even_and_number.py
@@ -0,0 +1,20 @@
+def evenOdd(numbers):
+    
+    even = []
+    odd = []
+    
+    for number in numbers:
+        if (int(number) % 2 == 0):
+            even.append(number)
+        else:
+            odd.append(number)
+    
+    return even, odd
+
+if __name__ == '__main__':
+    userInput = input('Enter the numbers (space separated) to check: ')
+    userInput = list(userInput.split())
+    even, odd = evenOdd(userInput)
+    print('Even nos: ', ', '.join(even), '\n', 'Odd nos: ', ', '.join(odd))
+    
+    
